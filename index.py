1233dedc5ee7d7af6a7a75d1195403cb5a8c0e26
Pogrammer <87412615+The-Pogrammer@users.noreply.github.com>
2023-09-18 18:10:44 -0400

    Add files via upload

--- index.py
+++ index.py
@@ -4,15 +4,11 @@ import random
 
 from discord.ext import tasks, commands
 from dotenv import load_dotenv
-from customExpressionHandler import expressionHandler
-from discordfunctions import get_user_info, get_guild_info
+from discordfunctions import *
 
 load_dotenv()
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix="p!")
 TOKEN = os.getenv('TOKEN')
-
-whitelist = [697959912302444614, 373266820792188928, 853754440509947914]
-blacklist = []
 
 @bot.event
 async def on_ready():
@@ -33,33 +29,55 @@ async def verifyuserexists(interaction: discord.Interaction, id: str):
         await interaction.response.send_message("user doesn't exist")
 
 @bot.tree.command()
-async def runprogram(interaction: discord.Interaction, code: str):
-    if interaction.user.id not in whitelist and interaction.user.id not in blacklist:
-        await interaction.response.send_message("Sorry, you can't use this command", ephemeral=True)
-        return
-    
-    print(interaction.user.display_name + ": " + code)
-
-    handler = expressionHandler()
-
-    splitcode = code.split(";")
-    for segment in splitcode:
-        try:
-            handler.intereptExpression(segment)
-        except Exception as e:
-            print(e)
-            await interaction.response.send_message("error :(", ephemeral=True)
-            return
-        
-    await interaction.response.send_message(str(handler))
-
-@bot.tree.command()
 async def furry(interaction: discord.Interaction):
     #get furrylinks.txt
     with open("furrylinks.txt") as file:
         links = file.readlines()
-        await interaction.response.send_message(links[random.randint(0, len(links)-1)])
+        link = links[random.randint(0, len(links)-1)]
 
+        await interaction.response.defer()
+
+        if (link.startswith("https:")):
+            await interaction.followup.send(link)
+        else:
+            file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "speechbubblefurries", link.strip().replace("file:", "", 1))
+
+            with open(file_path, "rb") as file:
+                await interaction.followup.send(file=discord.File(file))
+
+@bot.tree.command()
+async def checkchanneldescriptions(interaction: discord.Interaction):
+    #whitlist for only me :)
+    if interaction.user.id != 697959912302444614:
+        await interaction.response.send_message("Sorry, you can't use this command", ephemeral=True)
+        return
+    
+    channelinfo = []
+    for channel in interaction.guild.channels:
+        if isinstance(channel, discord.TextChannel):
+            description = channel.topic if channel.topic else "No description"
+            channelinfo.append(channel.name + ": " + description)
+    
+    print(channelinfo)
+
+
+    try:
+        #split into 2000 character segments and send seperately
+        message = "\n\n".join(channelinfo)
+        with open("channelinfo.txt", "w") as file:
+            #replace characters that can't be encoded
+            message = message.encode("ascii", "ignore").decode()
+            
+            file.write(message)
+            
+        await interaction.response.send_message(file=discord.File("channelinfo.txt"))
+
+        #delete file
+        os.remove("channelinfo.txt")
+
+
+    except Exception as e:
+        print(e)
 
 @bot.command()
 async def makemeasandwich(ctx):
